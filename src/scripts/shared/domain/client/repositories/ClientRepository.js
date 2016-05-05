ec1589fd011c52cecd88c81fe7b889bc8c57dfe2
Nate Geslin <teamtomkins23@gmail.com>
2016-05-05 18:24:06 -0500

    Adds saveClient repository test * Adds doc blocks to repository methods

--- src/scripts/shared/domain/client/repositories/ClientRepository.js
+++ src/scripts/shared/domain/client/repositories/ClientRepository.js
@@ -8,6 +8,12 @@ import { ENDPOINTS } from '../../endpoints';
 const ENDPOINT = `${ENDPOINTS}/clients`;
 
 export default {
+    /**
+     * Get a list of all `Clients`
+     *
+     * @function getClientList
+     * @return {ClientListType}
+     */
     getClientList: () => {
         return request.get(`${ENDPOINT}`)
             .then(response => new ClientListType(response.data))
@@ -16,22 +22,13 @@ export default {
             });
     },
 
-    createClient: createClientRequest => {
-        return request.post(`${ENDPOINT}`, createClientRequest)
-            .then(response => new ClientType(response.data))
-            .catch(error => {
-                throw error;
-            });
-    },
-
-    saveClient: (id, clientRequest) => {
-        return request.put(`${ENDPOINT}/${id}`, clientRequest)
-            .then(response => new ClientType(response.data))
-            .catch(error => {
-                throw error;
-            });
-    },
-
+    /**
+     * Get a single `Client`
+     *
+     * @function getSingleClient
+     * @param {Number} id  Client.id
+     * @return {ClientType}
+     */
     getSingleClient: id => {
         return request.get(`${ENDPOINT}/${id}`)
             .then(response => new ClientType(response.data))
@@ -40,6 +37,43 @@ export default {
             });
     },
 
+    /**
+     * Create a new `Client`
+     *
+     * @function createClient
+     * @param {ClientCreationType} createClientRequest
+     * @return {ClientType}
+     */
+    createClient: createClientRequest => {
+        return request.post(`${ENDPOINT}`, createClientRequest)
+            .then(response => new ClientType(response.data))
+            .catch(error => {
+                throw error;
+            });
+    },
+
+    /**
+     * Update an existing `Client`
+     *
+     * @function saveClient
+     * @param {Number} id  Client.id
+     * @param {ClientRequestType} clientRequest
+     * @return {ClientType}
+     */
+    saveClient: (id, clientRequest) => {
+        return request.put(`${ENDPOINT}/${id}`, clientRequest)
+            .then(response => new ClientType(response.data))
+            .catch(error => {
+                throw error;
+            });
+    },
+
+    /**
+     * Delete a single `Client`
+     *
+     * @function deleteClient
+     * @param {Number} id  Client.id
+     */
     deleteClient: id => {
         return request.delete(`${ENDPOINT}/${id}`)
             .then(response => response)
