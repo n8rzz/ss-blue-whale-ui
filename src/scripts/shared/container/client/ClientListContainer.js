f76a2f125546cc14aa63a73c202d606d0b3274e0
Nate Geslin <teamtomkins23@gmail.com>
2016-05-09 23:50:21 -0500

    Adds NoteType mocks, fixtures and test * Adds NoteType to ClientType

--- src/scripts/shared/container/client/ClientListContainer.js
+++ src/scripts/shared/container/client/ClientListContainer.js
@@ -17,6 +17,12 @@ export class ClientListContainer extends Component {
     render() {
         return (
             <div>
+                <ul>
+                    <li>
+                        <input type="text" name="s" placeholder="search" />
+                    </li>
+                </ul>
+
                 <ClientList clients={ this.props.clients } />
             </div>
         );
