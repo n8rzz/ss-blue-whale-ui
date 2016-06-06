ef09d2697abaf3fb0a9bfb9b06bf9be4d5529771
Nate Geslin <teamtomkins23@gmail.com>
2016-06-06 14:24:14 -0500

    Adds SessionTypes mocks, fixtures, and Type tests

--- spec/shared/domain/session/types/SessionTypes.spec.js
+++ spec/shared/domain/session/types/SessionTypes.spec.js
@@ -1,36 +1,36 @@
-// /* eslint-disable */
-// import ava from 'ava';
-//
-// import {
-//     RegistrationRequestType,
-//     RegistrationResponseType,
-//     RegistrationStateType
-// } from '../../../../../src/scripts/shared/domain/registration/types/RegistrationTypes';
-//
-// import {
-//     ValidRegistrationRequestType,
-//     ValidRegistrationResponseType,
-//     ValidRegistrationStateType
-// } from '../../../../specHelper/fixtures/registration/RegistrationFixtures';
-//
-// import {
-//     VALID_REGISTRATION_REQUEST,
-//     VALID_REGISTRATION_RESPONSE
-// } from '../../../../specHelper/mocks/registration/RegistrationMocks';
-//
-// ava('RegistrationRequestType', t => {
-//     t.throws(() => RegistrationRequestType(''));
-//     t.notThrows(() => RegistrationRequestType(VALID_REGISTRATION_REQUEST));
-//     t.notThrows(() => RegistrationRequestType.is(ValidRegistrationRequestType));
-// });
-//
-// ava('RegistrationResponseType', t => {
-//     t.throws(() => RegistrationResponseType(''));
-//     t.notThrows(() => RegistrationResponseType(VALID_REGISTRATION_RESPONSE));
-//     t.notThrows(() => RegistrationResponseType.is(ValidRegistrationResponseType));
-// });
-//
-// ava('ValidRegistrationStateType', t => {
-//     t.throws(() => RegistrationStateType(''));
-//     t.notThrows(() => RegistrationStateType.is(ValidRegistrationStateType));
-// });
+/* eslint-disable new-cap */
+import ava from 'ava';
+
+import {
+    SessionRequestType,
+    SessionResponseType,
+    SessionStateType
+} from '../../../../../src/scripts/shared/domain/session/types/SessionTypes';
+
+import {
+    ValidSessionRequestType,
+    ValidSessionResponseType,
+    ValidSessionStateType
+} from '../../../../specHelper/fixtures/session/SessionFixutes';
+
+import {
+    VALID_SESSION_CREATION_REQUEST,
+    VALID_SESSION_RESPONSE
+} from '../../../../specHelper/mocks/session/SessionMocks';
+
+ava('SessionRequestType', t => {
+    t.throws(() => SessionRequestType(''));
+    t.notThrows(() => SessionRequestType(VALID_SESSION_CREATION_REQUEST));
+    t.notThrows(() => SessionRequestType.is(ValidSessionRequestType));
+});
+
+ava('SessionResponseType', t => {
+    t.throws(() => SessionResponseType(''));
+    t.notThrows(() => SessionResponseType(VALID_SESSION_RESPONSE));
+    t.notThrows(() => SessionResponseType.is(ValidSessionResponseType));
+});
+
+ava('ValidSessionStateType', t => {
+    t.throws(() => SessionStateType(''));
+    t.notThrows(() => SessionStateType.is(ValidSessionStateType));
+});
--- src/scripts/shared/domain/session/types/SessionTypes.js
+++ src/scripts/shared/domain/session/types/SessionTypes.js
@@ -1,33 +1,31 @@
-// import t from 'tcomb';
-// import { BaseStateType } from '../../BaseTypes';
-//
-// /**
-//  * @type {RegistrationRequestType}
-//  * @return {RegistrationRequestType}
-//  */
-// export const RegistrationRequestType = t.struct({
-//     email: t.String,
-//     username: t.String,
-//     password: t.String,
-//     password_confirmation: t.String
-// }, 'RegistrationRequestType');
-//
-// /**
-//  * @type {RegistrationResponseType}
-//  * @return {RegistrationResponseType}
-//  */
-// export const RegistrationResponseType = t.struct({
-//     user_id: t.Number,
-//     email: t.String,
-//     username: t.String,
-//     access_token: t.String,
-//     token_type: t.String
-// }, 'RegistrationResponseType');
-//
-// /**
-//  * @type {RegistrationStateType}
-//  * @return {RegistrationStateType}
-//  */
-// export const RegistrationStateType = BaseStateType.extend({
-//     payload: t.maybe(RegistrationResponseType)
-// }, 'RegistrationStateType');
+import t from 'tcomb';
+import { BaseStateType } from '../../BaseTypes';
+
+/**
+ * @type SessionRequestType
+ * @return {SessionRequestType}
+ */
+export const SessionRequestType = t.struct({
+    email: t.String,
+    password: t.String
+}, 'SessionRequestType');
+
+/**
+ * @type SessionResponseType
+ * @return {SessionResponseType}
+ */
+export const SessionResponseType = t.struct({
+    user_id: t.Number,
+    email: t.String,
+    username: t.String,
+    access_token: t.String,
+    token_type: t.String
+}, 'SessionResponseType');
+
+/**
+ * @type SessionStateType
+ * @return {SessionStateType}
+ */
+export const SessionStateType = BaseStateType.extend({
+    payload: t.maybe(SessionResponseType)
+}, 'SessionStateType');
