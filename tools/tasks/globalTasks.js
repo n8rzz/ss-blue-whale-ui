f01f8dc8c5676eb954a528fe0ad7f5e33ef419b5
Nate Geslin <teamtomkins23@gmail.com>
2016-06-10 00:57:21 -0500

    Removes gulp release task from globalTasks file

--- tools/tasks/globalTasks.js
+++ tools/tasks/globalTasks.js
@@ -53,14 +53,6 @@ module.exports = function(gulp, config) {
     ////////////////////////////////////////////////////////////////////
     // TASKS
     ////////////////////////////////////////////////////////////////////
-    var runSequence = require('run-sequence');
 
     gulp.task('clean', ['clean:dest:assets']);
-    gulp.task('release', function() {
-        runSequence(
-            'build',
-            'bump',
-            'tag'
-        );
-    });
 }
